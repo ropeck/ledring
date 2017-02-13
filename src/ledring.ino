eb095e5bccdb94da60650bcb24fcd05a8a3d9a30
Rodney Peck <ropeck@google.com>
2017-02-12 20:18:45 -0800

    added debug publish

--- src/ledring.ino
+++ src/ledring.ino
@@ -3,24 +3,28 @@
 
 InternetButton b = InternetButton();
 NtpTime* ntpTime;
+char buf[255];
 
 void setup() {
     ntpTime = new NtpTime(15);  // Do an ntp update every 15 minutes;
     ntpTime->start();
 
     b.begin();
-//    b.allLightsOff();
 }
 
-int i = 0;
+#ifdef DEBUG
+ #define DEBUG_PRINT(fmt, ...) sprintf(buf, fmt, __VA_ARGS__); Particle.publish("debug",buf); 
+#else
+ #define DEBUG_PRINT(x)
+#endif
+
 
 void loop() {
     static unsigned long waitMillis = 0;
-    char buf[255];
+    static int i = 0;
 
     if(millis() > waitMillis) {
-        sprintf(buf, "Clock is: %d", Time.now());
-        Particle.publish(buf);
+        DEBUG_PRINT("Clock is: %d", Time.now());
         waitMillis = millis() + (1000);
     }
   // starting 30 minutes before the alarm, set the LED brightness
